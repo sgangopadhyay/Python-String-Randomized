e25a59b49e4838ccc8b8ee7f2a17029bd113983a
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2018-09-10 13:47:15 +0530

    Update random_string.py
    edit 1

--- random_string.py
+++ random_string.py
@@ -2,6 +2,7 @@
 # CODED BY : Suman Gangopadhyay
 # DATE OF CODING : 10-Sept-2018
 # CAVEATS : Using Python 3.5.2
+# Email ID : linuxgurusuman@gmail.com
 
 import random
 def Jumble_Word(word):    
@@ -9,4 +10,4 @@ def Jumble_Word(word):
     random.shuffle(word)
     return ''.join(word)
 
-print(Jumble_Word('noon'))
+print(Jumble_Word('SumanGangopadhyay'))
